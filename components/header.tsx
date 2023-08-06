7f3a9226ebc61debd695f483da54b6d21bd8dc88
NielReitmann <64618262+Niel-Reitmann@users.noreply.github.com>
2023-08-06 14:52:57 +1000

    redirect 'back'

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <h2 className="text-2xl sm:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight sm:mb-20 mt-8">
-      <Link href="/" className="hover:underline">
+      <Link href="/blogpage" className="hover:underline">
         Back
       </Link>
     </h2>
