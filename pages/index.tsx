ce5898ced45761d261991880748d1a080cbcc436
NielReitmann <64618262+Niel-Reitmann@users.noreply.github.com>
2024-11-18 10:40:54 +1100

    cleaned

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="w-full min-h-screen text-gray-500 flex flex-col justify-center items-center bg-white">
         <div className="flex flex-col w-1/1 sm:w-1/2 justify-center min-h-screen p-5 sm:px-5 gap-6 md:gap-10 md:px-0 ">
           <div className="text-3xl sm:text-5xl font-light leading-tight">
-            My name is Niel Reitmann. I'm currently a Associate Product Manager at{" "}
+            My name is Niel Reitmann. Currently Associate Product Manager at{" "}
             <a
               href="https://www.atlassian.com/"
               className="text-black hover:underline transition duration-100"
